a176097154a6c65d967e55d96a83aebbe171a221
bbloch <bbloch@veepee.com>
2024-04-29 10:50:13 +0200

    Initial Commit

---
+++ EPCManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EPCManager
+//
+//  Created by Bertrand Bloc'h on 29/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EPCManager/EPCManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EPCManagerApp.swift
+//  EPCManager
+//
+//  Created by Bertrand Bloc'h on 29/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct EPCManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
